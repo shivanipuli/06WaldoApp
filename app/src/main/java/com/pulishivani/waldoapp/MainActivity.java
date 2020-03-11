c29d10e7e84a6194f6f2e2f4aa2c4c2218f0bdd3
Shivani Puli <shivanipuli@gmail.com>
2020-03-11 13:30:44 -0400

    Lab06d: Finished Waldo App. Now have 6 pages and completely functional. Made SharedPreferences work. Just need to add GSON now

--- app/src/main/java/com/pulishivani/waldoapp/MainActivity.java
+++ app/src/main/java/com/pulishivani/waldoapp/MainActivity.java
@@ -9,13 +9,14 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    int[] layouts={R.layout.waldo1,R.layout.waldo2,R.layout.waldo3,R.layout.waldo4,R.layout.waldo5};
+    int[] layouts={R.layout.waldo1,R.layout.waldo2,R.layout.waldo3,R.layout.waldo4,R.layout.waldo5,R.layout.waldo6};
     int count=0;
     SharedPreferences sharedPreferences;
     SharedPreferences.Editor editor;
-    int[] header={R.id.textView1,R.id.textView2,R.id.textView3,R.id.textView4,R.id.textView5};
+    int[] header={R.id.textView1,R.id.textView2,R.id.textView3,R.id.textView4,R.id.textView5,R.id.textView6};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,35 +24,44 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         editor = sharedPreferences.edit();
-        for(int x=0;x<header.length;x++){
+        for(int x=1;x<=header.length;x++){
             editor.putString(x+"","Level " + x + ": Incomplete");
             editor.apply();
         }
     }
-    public void next(View view){
-        editor.putString(count+"","Level " + count + ": Completed");
+    public void found(View view){
+        int x=count+1;
+        editor.putString(x+"","Level " + x + ": Completed");
         editor.apply();
+        TextView head=findViewById(header[count]);
+        head.setText(sharedPreferences.getString(x+"","Incomplete"));
+        Toast.makeText(getApplicationContext(),"You Found Him!",Toast.LENGTH_SHORT).show();
+    }
+    public void next(View view){
         count++;
         if(count>=layouts.length){
             count=0;
         }
         setContentView(layouts[count]);
+        int x=count+1;
         TextView head=findViewById(header[count]);
-        head.setText(sharedPreferences.getString(count+"","Incomplete"));
+        head.setText(sharedPreferences.getString(x+"","Incomplete"));
         Log.i("next", "count=" + count);
     }
-    public void nextt(View view){
-        count++;
-        if(count>=layouts.length){
-            count=0;
+    public void prev(View view){
+        count--;
+        if(count<0){
+            count=count+layouts.length;
         }
         setContentView(layouts[count]);
+        int x=count+1;
         TextView head=findViewById(header[count]);
-        head.setText(sharedPreferences.getString(count+"","Incomplete"));
+        head.setText(sharedPreferences.getString(x+"","Incomplete"));
         Log.i("next", "count=" + count);
     }
     public void home(View view){
         setContentView(R.layout.activity_main);
+        count--;
     }
 
 }
