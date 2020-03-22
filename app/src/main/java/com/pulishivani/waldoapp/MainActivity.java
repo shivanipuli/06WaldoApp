8088904b082983899082445af4ccb7d97f1384d3
Shivani <shivanipuli@gmail.com>
2020-03-22 12:39:07 -0400

    Lab06e: Added fragment and JSON, done with lab

--- app/src/main/java/com/pulishivani/waldoapp/MainActivity.java
+++ app/src/main/java/com/pulishivani/waldoapp/MainActivity.java
@@ -11,12 +11,16 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.gson.Gson;
+
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
-    int[] layouts={R.layout.waldo1,R.layout.waldo2,R.layout.waldo3,R.layout.waldo4,R.layout.waldo5,R.layout.waldo6};
+    int[] layouts={R.layout.waldo1,R.layout.waldo2,R.layout.waldo3,R.layout.waldo4,R.layout.waldo5};
     int count=0;
     SharedPreferences sharedPreferences;
     SharedPreferences.Editor editor;
-    int[] header={R.id.textView1,R.id.textView2,R.id.textView3,R.id.textView4,R.id.textView5,R.id.textView6};
+    int[] header={R.id.textView1,R.id.textView2,R.id.textView3,R.id.textView4,R.id.textView5};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
         int x=count+1;
         TextView head=findViewById(header[count]);
         head.setText(sharedPreferences.getString(x+"","Incomplete"));
-        Log.i("next", "count=" + count);
+
     }
     public void prev(View view){
         count--;
@@ -58,6 +62,12 @@ public class MainActivity extends AppCompatActivity {
         TextView head=findViewById(header[count]);
         head.setText(sharedPreferences.getString(x+"","Incomplete"));
         Log.i("next", "count=" + count);
+    }
+    public void json(View view){
+        Button btn = findViewById(R.id.test);
+        Gson gson = new Gson();
+        String json = gson.toJson(btn);
+        Log.i("btn", json);
     }
     public void home(View view){
         setContentView(R.layout.activity_main);
